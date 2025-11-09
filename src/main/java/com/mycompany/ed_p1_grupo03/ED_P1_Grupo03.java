4fcbd2cad4bb0e38f052d50b154dd332f1f5decc
Anahi Quezada <70460216+Anie2490@users.noreply.github.com>
2025-11-08 19:18:34 -0500

    Commit de prueba
    Ignorar

--- src/main/java/com/mycompany/ed_p1_grupo03/ED_P1_Grupo03.java
+++ src/main/java/com/mycompany/ed_p1_grupo03/ED_P1_Grupo03.java
@@ -12,5 +12,6 @@ public class ED_P1_Grupo03 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Prueba prueba prueba");
     }
 }
